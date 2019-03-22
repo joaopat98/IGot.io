e0faad0a5895880aba3e9e72eabb436c420bb27c
Fábio Cordeiro <uc2016242575@student.uc.pt>
2019-03-22 22:51:39 +0000

    db accounts

--- server/game/models.py
+++ server/game/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True,related_name="user_profile")
+    username = models.CharField(max_length=240)
+    phone_number = models.CharField(max_length=20,blank=True)
+    max_score = models.IntegerField()
+
+    def serialize(self):
+        u = p.user
+        p = self
+        return{
+            "user_id": u.id,
+            "username": p.username,
+            "phone_number": p.phone_number,
+            "max_score": p.max_score,
+        }
+
+
+class Skin(models.Model):
+    id = models.IntegerField(primary_key=True)
+    profile = models.ManyToManyField(Profile,through="UserSkins")
+    path_png = models.CharField(max_length=240)
+
+    def serialize(self):
+        s = self
+        return{
+            "skin_id": s.id,
+            "path": s.path_png, 
+        }
+
+class UserSkins(models.Model):
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    skin = models.ForeignKey(Skin,on_delete=models.CASCADE)
