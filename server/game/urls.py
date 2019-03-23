f46913e2f86533b6483ad1aa3e58abcfeadf9dbc
unknown <mike.gonzaga1998@gmail.com>
2019-03-23 05:26:06 +0000

    Add homepage render with args

--- server/game/urls.py
+++ server/game/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 
 from .views import *
 
 urlpatterns = [
     path("join", join),
     path("login", login_session),
-    path("register", register)
+    path("register", register),
 ]
